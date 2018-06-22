c0327a916564edf631f00683a8eb71f30168371a
Michelle <39346816+DekuPrincess@users.noreply.github.com>
2018-06-22 17:03:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-const nav = document.querySelector('#main');
-let topOfNav = nav.offsetTop;
-    
-function fixNav() {
-  if(document.documentElement.clientWidth >= 960){  //only use effect if screen is larger than 960px
-    if (window.scrollY >= topOfNav) {
-      document.body.style.paddingTop = nav.offsetHeight + 'px';
-      document.body.classList.add('fixed-nav');
-        } else {
-          document.body.classList.remove('fixed-nav');
-          document.body.style.paddingTop = 0;
-        }
-      }
-    }
 
-window.addEventListener('scroll', fixNav);
 
 //smooth scrolling for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
